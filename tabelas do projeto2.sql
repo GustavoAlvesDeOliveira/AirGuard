d7f367a1d890d8657fad616d7b3f5a1ea741e39b
CelinaBenedito <celina.benedito@sptech.school>
2025-03-17 21:40:00 +0000

    Atualizações no banco de dados

--- tabelas do projeto2.sql
+++ tabelas do projeto2.sql
@@ -1,29 +1,31 @@
-create database  airguard;
+create database airguard;
 use airguard;
 
 create table Empresas ( 
-idEmpresa int not null primary key auto_increment,
+IdEmpresa int not null primary key auto_increment,
 NomeEmpresa varchar(50) NOT NULL,
 NomeRepresentante varchar(100) NOT NULL,
-Endereco varchar(500) NOT NULL,
-cep char(08) NOT NULL,
-Cnpj varchar(20) NOT NULL,
-qtdsensores int
+Logradouro varchar(200) NOT NULL,
+Bairro varchar(30) NOT NULL,
+Cep char(08) NOT NULL,
+Cnpj varchar(20) NOT NULL
 );
+
 create table Sensores( 
-idSensores int not null primary key auto_increment,
-qtd int NOT NULL,
-localização varchar(20),
-fkempresa int,
+IdSensores int not null primary key auto_increment,
+Qtd int NOT NULL,
+Localização varchar(20) NOT NULL,
+FkEmpresa int,
 constraint ck_qtd check(qtd>0)
 )auto_increment=100;
+
 create table registros ( 
 volume int,
 horario timestamp default current_timestamp,
 fkSensores int
 );
+
 alter table registros add foreign key(fksensores) references Sensores(idSensores);
-alter table Empresas add foreign key(qtdsensores) references Sensores(idSensores);
 alter table Sensores add foreign key(fkempresa) references Empresas(idEmpresa);
 
 create unique index ix_cep on Empresas(cep);
